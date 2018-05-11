185c02503448b5afcf3b6aace9ca5fc667466877
Lau Kar Rui <karrui.lau@gmail.com>
2018-05-11 21:47:06 +0800

    Add SearchBar and VideoList classes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAMNOXgPAaEGfLOCe5PgqSL6hdbXNGnDIY';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'anime'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// import App from './components/app';
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+
+// import reducers from './reducers';
+
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <App />
+//   </Provider>
+//   , document.querySelector('.container'));
